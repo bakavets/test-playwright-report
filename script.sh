a810db28b04e0ed5917ef26590c784b7f5af1e79
Anton Bakavets <bakavets.com@gmail.com>
2024-05-23 09:52:54 +0200

    INit

--- script.sh
+++ script.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+
+while [ $# -gt 0 ]; do
+    if [[ $1 == "--"* ]]; then
+        v="${1/--/}"
+        declare "$v"="$2"
+        shift
+    fi
+    shift
+done
+
 source /root/.nvm/nvm.sh
-echo "[INFO] Test Suite name ${3}";
+echo "[INFO] Test Suite name ${testsuite}";
 npx playwright --version
-npx playwright test --shard=${1}/${2}
+npx playwright test --shard=${shard}/${totalshards}
